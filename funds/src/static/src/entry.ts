0d6907c402cfb085ba7ca834132f40bcfdd791ea
Yang SHEN <biztudio@outlook.com>
2017-11-25 23:34:44 +0800

    add comment about declaration in Jest-based test for typescript.

--- funds/src/static/src/entry.ts
+++ funds/src/static/src/entry.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 //import iView from 'iview';
 import * as _ from 'lodash';
+import { RandomTool } from './common/random';
+
+let rTool = new RandomTool(10);
+console.log(rTool.get_random_number());
 
 let v = new Vue({
     el: "#app",
--- funds/src/static/test/random.test.ts
+++ funds/src/static/test/random.test.ts
@@ -1,5 +1,15 @@
+/*
+  [需要首先进行环境声明]
+  * 环境声明向TypeScript域中引入一个变量，这对生成的JS里将没有任何影响;
+  * 不指定类型，将默认采用 any 类型；
+  * document等JS内建的对象通过文件‘lib.d.ts’默认包含在TS中;
+    举例：可以这样引入jQuery库
+          declare var $;
+*/
 declare var jest, describe, it, test, expect, beforeAll, afterAll, beforeEach, afterEach;
-
+/*
+引入被测试模块
+*/
 import { RandomTool } from '../src/common/random';
 
 describe('Test Class RandomTool', () =>{
