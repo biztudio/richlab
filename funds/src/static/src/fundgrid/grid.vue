49594e20b766c5e8a2a190cb9f90cb9e08d03f34
Yang SHEN <biztudio@outlook.com>
2017-11-28 23:29:43 +0800

    set table features

--- funds/src/static/src/fundgrid/grid.vue
+++ funds/src/static/src/fundgrid/grid.vue
@@ -4,20 +4,29 @@
         
         <el-table
             :data="tableData"
+            height="500"
             stripe
+            :default-sort = "{prop: 'name', order: 'ascending'}"
             style="width: 100%">
             <el-table-column
+             type="index"
+             :index="indexMethod">
+            </el-table-column>
+            <el-table-column
             prop="code"
+            sortable
             label="基金代码"
             width="180">
             </el-table-column>
             <el-table-column
             prop="name"
+            sortable
             label="基金名称"
             width="500">
             </el-table-column>
             <el-table-column
             prop="fee"
+            sortable
             label="费率">
             </el-table-column>
         </el-table>
@@ -42,6 +51,12 @@
 
         created:function(){
             this.tableData = this.fund_model.list_fund();       
+        },
+
+        methods:{
+             indexMethod(index:number) {
+                    return index + 1;//demo
+                }
         }
         
     });
