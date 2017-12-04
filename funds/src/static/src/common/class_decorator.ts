b3e65fb248aa2d8c404bc29f372698502c989365
Yang SHEN <biztudio@outlook.com>
2017-12-05 00:00:38 +0800

    update comment.

--- funds/src/static/src/common/class_decorator.ts
+++ funds/src/static/src/common/class_decorator.ts
@@ -4,7 +4,7 @@ import { Tree } from "element-ui";
 /*
  * 方法装饰器 参数
         target 对于静态成员来说是类的构造函数,对于实例成员是类的原型对象
-        propertyKey 成员的名字  这里是method
+        key 成员的名字  这里是method
         descriptor 成员的属性描述符
  如果方法装饰器返回一个值 ,它会被用作方法的塑形描述符
  */
