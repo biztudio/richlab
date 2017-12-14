1641caf82e3d6090939047bba91457f73f4a7021
Yang SHEN <yang.shen@commscope.com>
2017-12-14 17:18:09 +0800

    list manager name

--- funds/src/service/easymoney/fund_manager_data.py
+++ funds/src/service/easymoney/fund_manager_data.py
@@ -5,7 +5,7 @@ class FundManager():
     '''
 
     '''
-    def __init__(self, name, company):
+    def __init__(self, name, ref_link, managing_funds):
         pass
 
 class FundManagerService():
@@ -21,4 +21,24 @@ class FundManagerService():
         '''
         根据基金代码找出该基金对应的过往基金经理及他们管理的基金
         '''
-        pass    
+        page = urllib.request.urlopen('http://fund.eastmoney.com/f10/jjjl_'+ fund_code +'.html')
+        lines = page.readlines()
+        page.close()
+        document = ''
+        for line in lines :
+            document = document + line.decode('utf-8')
+        #print(document)    
+        soup = BeautifulSoup(document, "html.parser")
+        tables = soup.find_all('table', class_='w782 comm jloff')
+        managers_table = tables[0]
+        managers = [mr.find_all('td')[2].text for mr in managers_table.find_all('tr')[1:]]
+
+        #注意有很多基金有多个基金经理，所以关联基金列表应该有多个
+        #related_funds_to_current_manager = tables[1]
+        print(managers)
+
+if __name__ == '__main__':
+    fund_code = '110022'
+    service = FundManagerService()
+    result = service.fetch_manager_info(fund_code)
+
