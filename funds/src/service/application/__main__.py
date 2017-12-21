6e242c37fcf3855f7c93c531758cc38f9e228e1f
Yang SHEN <biztudio@outlook.com>
2017-12-21 23:02:30 +0800

    download all fund perf data.

--- funds/src/service/application/__main__.py
+++ funds/src/service/application/__main__.py
@@ -1,19 +1,35 @@
 #from range_service import RangeService, RangeResult
 from application.range_service import RangeService
+from tinydb import TinyDB
+from datetime import date
+
+today_text = date.today().strftime('%Y%m%d')
 
 def get_all_fund_code():
-    from tinydb import TinyDB
     db = TinyDB('data/fundlist_db.json')
     fund_table = db.table('fund')
     return [f['code'] for f in fund_table.all()]
 
-#002906
-fund_codes_list = ['002906','002620','110022','000706']#get_all_fund_code()
-service = RangeService(fund_codes_list)
-worst_list_500 = service.find_worst_top500_funds()
-best_list_500 = service.find_best_top500_funds()
-print('BEST 500')
-print(best_list_500)
+def archive_fund_performance(archive_name, fund_perf_list):
+    print('Archiving to data/'+archive_name+'.json...')
+    perf_db = TinyDB('data/'+archive_name+'.json')
+    perf_fund_table = perf_db.table('fund')
+    perf_db.purge_table('fund')
+    perf_fund_table.insert_multiple({
+        'code':f.fund_code, 'name':f.fund_name, 'category':f.category_text,
+        'score':f.score, 'percent':f.score_percent, 
+        'suggestion':f.suggestion, 'conclude':f.conclude_statement
+    } for f in fund_perf_list)
+    print('Archived.')
 
-print('WORST 500')
-print(worst_list_500)
+def list_fund_performance_archive(archive_name):
+    perf_db = TinyDB('data/'+archive_name+'.json')    
+    fund_table = perf_db.table('fund')
+    return fund_table.all()
+
+#002906
+fund_codes_list = ['002906','002620','110022','000706','501021','002330']#get_all_fund_code()#
+service = RangeService(fund_codes_list)
+all_fund_perform_list = service.load_all_fund_perform_info()
+archive_fund_performance('all_fund_perform_list_'+today_text, all_fund_perform_list)
+#print(list_fund_performance_archive('all_fund_perform_list_'+today_text))
--- funds/src/service/dkhs/manager_perf_on_fund.py
+++ funds/src/service/dkhs/manager_perf_on_fund.py
@@ -9,15 +9,17 @@ from bs4 import BeautifulSoup
 class ManagerPerformance(object):
     '''
     '''
-    def __init__(self, fund_code, score, score_percent, suggestion, conclude_statement):
+    def __init__(self, fund_code, score, score_percent, suggestion, conclude_statement, fund_name, category_text):
         self.fund_code = fund_code
         self.score = score
         self.score_percent = score_percent
         self.suggestion = suggestion
         self.conclude_statement = conclude_statement
+        self.fund_name = fund_name
+        self.category_text = category_text
 
     def __repr__(self): 
-        return repr((self.fund_code, self.score, self.score_percent, self.suggestion, self.conclude_statement)) 
+        return repr((self.fund_code, self.fund_name, self.category_text, self.score, self.score_percent, self.suggestion, self.conclude_statement)) 
 
 class ManagerPerformanceService(object):
     '''
@@ -34,6 +36,9 @@ class ManagerPerformanceService(object):
         score = 0
         score_percent = '0'
         conclude_statement = ''
+        category_text = ''
+        fund_name = ''
+        suggestion = ''
         if(lines):
             for line in lines :
                 document = document + line.decode('utf-8')
@@ -41,11 +46,13 @@ class ManagerPerformanceService(object):
             soup = BeautifulSoup(document, "html.parser")           
             score_tag_beginning = soup.find('div',text='综合评分')#有的扩展属性不能直接用于搜索
             if(score_tag_beginning):
-                score_tag = score_tag_beginning.find_next('span')
-                score_percent_tag = score_tag.find_next('div').find('span')
-                score = score_tag.text
-                score_percent = score_percent_tag.text
-            suggestion = ''
+                score_tag = score_tag_beginning.find_next('span')                
+                if(score_tag):
+                    if(score_tag.text != '' and score_tag.text != '--'):
+                        score = int(score_tag.text)
+                    score_percent_tag = score_tag.find_next('div').find('span')
+                    if(score_percent_tag):
+                        score_percent = score_percent_tag.text
             ref_tag = soup.find('div', class_='hint', text='操作建议')
             if(ref_tag):
                 suggestion_tag = ref_tag.find_previous('div').find('span')
@@ -56,7 +63,19 @@ class ManagerPerformanceService(object):
                 conclude_tag = ref_tag.find_next('div')
                 if(conclude_tag):
                     conclude_statement = conclude_tag.text.strip()
-        performance = ManagerPerformance(fund_code, score, score_percent, suggestion, conclude_statement)
+            title_tag = soup.find('div', class_='panel-heading')
+            if(title_tag):
+                name_tag = title_tag.find('strong', class_='text-xg')
+                if(name_tag):
+                    fund_name = name_tag.text
+                category_tag = title_tag.find('span', class_="badge badge-light")
+                if(category_tag):
+                    '''
+                    混合型 股票型 指数型 债券型 货币型 QDII
+                    '''
+                    category_text = category_tag.text
+        performance = ManagerPerformance(fund_code, score, score_percent, suggestion, conclude_statement, fund_name, category_text)
+        print(fund_code,' done)
         return performance
 
 
