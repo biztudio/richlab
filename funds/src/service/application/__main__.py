f673014da66ad0724278ddda000f572907781b1e
Yang SHEN <biztudio@outlook.com>
2017-12-22 23:15:23 +0800

    fetch some data on Oct22

--- funds/src/service/application/__main__.py
+++ funds/src/service/application/__main__.py
@@ -1,5 +1,7 @@
 #from range_service import RangeService, RangeResult
 from application.range_service import RangeService
+from dkhs.manager_perf_on_fund import ManagerPerformance, ManagerPerformanceService
+
 from tinydb import TinyDB
 from datetime import date
 
@@ -22,14 +24,67 @@ def archive_fund_performance(archive_name, fund_perf_list):
     } for f in fund_perf_list)
     print('Archived.')
 
+def archive_fund_category_performance(archive_name, fund_perf_list):
+    print('Archiving to data/'+archive_name+'.json...')
+    perf_db = TinyDB('data/'+archive_name+'.json')
+    perf_fund_table = perf_db.table('fund')
+    perf_db.purge_table('fund')
+    perf_fund_table.insert_multiple({
+        'code':f['code'], 'name':f['name'], 'category':f['category'],
+        'score':f['score'], 'percent':f['percent'], 
+        'suggestion':f['suggestion'], 'conclude':f['conclude']
+    } for f in fund_perf_list)
+    print('Archived.')    
+
 def list_fund_performance_archive(archive_name):
     perf_db = TinyDB('data/'+archive_name+'.json')    
     fund_table = perf_db.table('fund')
     return fund_table.all()
 
+def load_all_fund_perform_info(fund_codes_list):
+    '''
+    download all the performance data from web
+    '''
+    fund_performance_list = []
+    mp_service = ManagerPerformanceService()
+    if(len(fund_codes_list) > 0):
+        print('loading funds...')
+        fund_performance_list = list(filter(lambda f:f.score > 0, [mp_service.get_performance_on_fund(f) for f in fund_codes_list]))
+        print('funds loaded')
+    return fund_performance_list
+
+
 #002906
 fund_codes_list = get_all_fund_code()#['002906','002620','110022','000706','501021','002330']#
 service = RangeService(fund_codes_list)
-all_fund_perform_list = service.load_all_fund_perform_info()
-archive_fund_performance('all_fund_perform_list_'+today_text, all_fund_perform_list)
-#print(list_fund_performance_archive('all_fund_perform_list_'+today_text))
+#all_fund_perform_list = load_all_fund_perform_info(fund_codes_list)
+#archive_fund_performance('all_fund_perform_list_'+today_text, all_fund_perform_list)
+service.fund_performance_list = list_fund_performance_archive('all_fund_perform_list_'+today_text)
+'''
+best_top500_index_funds = service.find_best_top500_funds(['指数型'])
+archive_fund_category_performance('best_top500_index_funds_'+today_text, best_top500_index_funds)
+
+best_top500_money_funds = service.find_best_top500_funds(['货币型'])
+archive_fund_category_performance('best_top500_money_funds_'+today_text, best_top500_money_funds)
+
+best_top500_bond_funds = service.find_best_top500_funds(['债券型'])
+archive_fund_category_performance('best_top500_bond_funds_'+today_text, best_top500_bond_funds)
+
+best_top500_allstock_funds = service.find_best_top500_funds(['混合型','股票型','指数型'])
+archive_fund_category_performance('best_top500_allstock_funds_'+today_text, best_top500_allstock_funds)
+
+best_top500_qdii_funds = service.find_best_top500_funds(['QDII'])
+archive_fund_category_performance('best_top500_qdii_funds_'+today_text, best_top500_qdii_funds)
+
+b500_allstock_funds = list_fund_performance_archive('best_top500_allstock_funds_'+today_text)
+print(b500_allstock_funds)
+
+b500_index_funds = list_fund_performance_archive('best_top500_index_funds_'+today_text)
+print(b500_index_funds)
+
+best_top500_allstock_excl_index_funds = service.find_best_top500_funds(['混合型','股票型'])
+archive_fund_category_performance('best_top500_allstock_excl_index_funds_'+today_text, best_top500_allstock_excl_index_funds)
+print(list_fund_performance_archive('best_top500_allstock_excl_index_funds_'+today_text))
+'''
+b500_qdii_funds = list_fund_performance_archive('best_top500_qdii_funds_'+today_text)
+print(b500_qdii_funds)
--- funds/src/service/application/range_service.py
+++ funds/src/service/application/range_service.py
@@ -1,7 +1,6 @@
 '''
 Services to find the best top 20 & worst top 20
 '''
-from dkhs.manager_perf_on_fund import ManagerPerformance, ManagerPerformanceService
 from easymoney.result_rawdata import ResultRawDataService, ResultRawDataModel
 from datetime import date
 #import asyncio
@@ -18,34 +17,21 @@ class RangeResult(object):
 class RangeService(object):
     def __init__(self, fund_codes_list):
         self.fund_codes_list = fund_codes_list
-        self.mp_service = ManagerPerformanceService()
         self.fund_performance_list = []
-
-    def load_all_fund_perform_info(self):
-        '''
-        download all the performance data from web
-        '''
-        if(len(self.fund_codes_list) > 0 and len(self.fund_performance_list) <= 0):
-            print('loading funds...')
-            self.fund_performance_list = list(filter(lambda f:f.score > 0, [self.mp_service.get_performance_on_fund(f) for f in self.fund_codes_list]))
-            print('funds loaded')
-        return self.fund_performance_list
 
     def find_best_top500_funds(self, fund_category = ['混合型','股票型','指数型','债券型','货币型','QDII']):
         '''
         To return the funds and the managers
         '''
-        self.load_all_fund_perform_info()
-        good_fund_list = list(filter(lambda f:((f.suggestion == '建议增持' or f.suggestion == '建议买入') and f.category_text in fund_category), self.fund_performance_list))
-        best_funds_top500 = sorted(good_fund_list, key=lambda f: f.score, reverse=True)[0:499]
+        good_fund_list = list(filter(lambda f:((f['suggestion'] == '建议增持' or f['suggestion'] == '建议买入') and f['category'] in fund_category), self.fund_performance_list))
+        best_funds_top500 = sorted(good_fund_list, key=lambda f: f['score'], reverse=True)[0:499]
         return best_funds_top500
 
     def find_worst_top500_funds(self, fund_category = ['混合型','股票型','指数型','债券型','货币型','QDII']):
         '''
         performance.suggestion == '建议减持' / '建议观望'
         '''
-        self.load_all_fund_perform_info()
-        bad_fund_list = list(filter(lambda f:((f.suggestion == '建议减持' or f.suggestion == '建议观望') and f.category_text in fund_category), self.fund_performance_list))
+        bad_fund_list = list(filter(lambda f:((f.suggestion == '建议减持' or f.suggestion == '建议观望') and f.category in fund_category), self.fund_performance_list))
         worst_funds_top500 = sorted(bad_fund_list, key=lambda f: f.score, reverse=False)[0:499]
         return worst_funds_top500
 
