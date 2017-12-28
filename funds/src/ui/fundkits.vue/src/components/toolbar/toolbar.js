67caf311d9bd2a887df3ebd717a515475786362e
Yang SHEN <biztudio@outlook.com>
2017-12-28 23:35:21 +0800

    basic ui structure.

--- funds/src/ui/fundkits.vue/src/components/toolbar/toolbar.js
+++ funds/src/ui/fundkits.vue/src/components/toolbar/toolbar.js
@@ -1,16 +1,22 @@
+import { fail } from "assert";
+
 /*jshint esversion: 6 */
 export default{
     data(){
         return {
             bar_items:[
-                {'name':'基金诊断', 'link':'#','badge':0,
-                 'icon_style':{'icon-home':true}},
-                {'name':'自选基金', 'link':'#','badge':0,
-                'icon_style':{'icon-cart':true}},
-                {'name':'业绩Top100', 'link':'#','badge':0,
-                'icon_style':{'icon-star':true}},
-                {'name':'关于', 'link':'#','badge':0,
-                'icon_style':{'icon-me':true}}
+                {'name':'基金诊断', 'link':'#diagnosis','badge':0,
+                 'icon_style':{'icon-home':true},
+                 'active_style':{'active':true}},
+                {'name':'自选基金', 'link':'#myfund','badge':0,
+                'icon_style':{'icon-cart':true},
+                'active_style':{'active':false}},
+                {'name':'业绩Top100', 'link':'#top100','badge':0,
+                'icon_style':{'icon-star':true},
+                'active_style':{'active':false}},
+                {'name':'关于我', 'link':'#about','badge':0,
+                'icon_style':{'icon-me':true},
+                'active_style':{'active':false}}
             ]
         }
     }
