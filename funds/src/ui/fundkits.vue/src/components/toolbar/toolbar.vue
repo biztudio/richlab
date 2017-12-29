08a29ddc8783fa88ff269ea102dc9b73ae01a01e
Yang SHEN <yang.shen@commscope.com>
2017-12-29 12:54:32 +0800

    remove 'external' to enable SPA ;mode.

--- funds/src/ui/fundkits.vue/src/components/toolbar/toolbar.vue
+++ funds/src/ui/fundkits.vue/src/components/toolbar/toolbar.vue
@@ -2,7 +2,7 @@
 <!--<id :id='toolbar_id'>-->
   
    <nav class="bar bar-tab">
-        <a v-for='(bi, bix) in bar_items' class="tab-item external" v-bind:class="bi.active_style" v-bind:href="bi.link">
+        <a v-for='(bi, bix) in bar_items' class="tab-item " v-bind:class="bi.active_style" v-bind:href="bi.link">
             <span class="icon" v-bind:class="bi.icon_style"></span>
             <span class="tab-label">{{bi.name}}</span>
             <span class="badge" v-if="bi.badge > 0">{{bi.badge}}</span>
