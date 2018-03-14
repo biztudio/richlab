2b4ee465b1b6ab6bcbd7373721a033a23b3455af
Yang SHEN <biztudio@outlook.com>
2018-03-15 00:04:10 +0800

    update test code

--- funds/src/ui/fundkits.vue/test/fundkits.data.fundperform.test.js
+++ funds/src/ui/fundkits.vue/test/fundkits.data.fundperform.test.js
@@ -1,7 +1,23 @@
 /*jshint esversion: 6 */
 import {FundPerformModel,FundPerform} from '../src/data/fund_perform_model.js';
 
+
+
 describe('Test Fund Perform Model Kit', ()=>{
+    let extendMethods = {
+        test:function(){
+          let t = [1,2,3,4,5]
+          
+           return t[4]
+        }
+      } 
+
+    test('TM',()=>{
+
+        console.log(extendMethods.test());
+        expect(1).toBe(1)
+
+    });
   
     test('Test Fund Perform Mode basic feature', ()=>{
         
